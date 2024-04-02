97eab564beb0f399fd78425f7c13478e5f3e7663
Yuchen Nie <ynie@salesforce.com>
2024-04-02 09:32:52 -0600

    Initial commit

---
+++ SwiftUIIntergrationProject/MainMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftUIIntergrationProject
+//
+//  Created by Yuchen Nie on 4/2/24.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
