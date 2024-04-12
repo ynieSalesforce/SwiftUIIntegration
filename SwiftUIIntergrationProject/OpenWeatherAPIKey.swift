f79f6db3a3676806e4c6d7898a68fc5e228ed7cb
Yuchen Nie <ynie@salesforce.com>
2024-04-11 20:07:05 -0600

    updated content

---
+++ SwiftUIIntergrationProject/OpenWeatherAPIKey.swift
@@ -0,0 +1,11 @@
+//
+//  OpenWeatherAPIKey.swift
+//  SwiftUIIntergrationProject
+//
+//  Created by Yuchen Nie on 4/3/24.
+//
+
+import Foundation
+
+let openWeatherAPIKey: String = "" //Enter your Open weather API Key from: https://home.openweathermap.org/api_keys
+
